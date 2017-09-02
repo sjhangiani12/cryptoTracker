874025be7ac28235f20ad40a7c7277f7d760fe1d
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2017-09-01 21:08:44 -0700

    initial commit

---
+++ cryptoTracker.py
@@ -0,0 +1,38 @@
+from pypoloniex import LoadPairs
+
+data = LoadPairs()
+
+
+LTC = data.getPair(market = 'USDT', coin = 'LTC')
+
+LTCvalue = float(LTC.last) * 1.3178
+
+LTCinvestment = 122.99 
+
+LTCnet = LTCvalue - LTCinvestment
+
+print 'Litecoin Net = %s ' %(LTCnet)
+
+BTC = data.getPair(market = 'USDT', coin = 'BTC')
+
+BTCvalue = float(BTC.last) * 0.021749
+
+BTCinvestment = 102.99
+
+BTCnet = BTCvalue - BTCinvestment
+
+print 'Bitcoin Net = %s ' %(BTCnet)
+
+ETH = data.getPair(market = 'USDT', coin = 'ETH')
+
+ETHvalue = float(ETH.last) * 0.25785 
+
+ETHinvestment = 102.99
+
+ETHnet = ETHvalue - ETHinvestment
+
+print 'Ethereum Net = %s ' %(ETHnet)
+
+totalNet = ETHnet + LTCnet + BTCnet
+
+print 'Total Investment Net Return = %s' %(totalNet)
